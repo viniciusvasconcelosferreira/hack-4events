d71dea4d9664a73ce6a2f21c00e57cb3f813060d
Ravanz <rayteon.h@gmail.com>
2023-04-08 11:44:12 -0300

    feat: cria circulos de acordo com os interesses no banco de dados

--- src/pages/Interests/interests.js
+++ src/pages/Interests/interests.js
@@ -1,9 +1,25 @@
-import React from "react";
-import {Input} from "@mui/joy";
+import React, { useState, useEffect } from "react";
+import { Input } from "@mui/joy";
 import SearchIcon from '@mui/icons-material/Search';
 import './styles.css';
 
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../../services/firebase';
+
+
 export default function Interests() {
+    const [comunidade, setComunidade] = useState([]);
+
+    useEffect(() => {
+        const fetchInterests = async () => {
+            const querySnapshot = await getDocs(collection(db, 'comunidade'));
+            const newInterests = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+            setComunidade(newInterests);
+            console.log(comunidade, newInterests);
+        };
+        fetchInterests();
+    }, [comunidade]);
+
     return (
         <section className={`interests__container`}>
             <div className={`interests__container--header`}>
@@ -13,16 +29,30 @@ export default function Interests() {
             <div className={`interests__container--sub-header`}>
                 <Input
                     className={`interests__container--sub-header__search`}
-                    startDecorator={<SearchIcon/>}
+                    startDecorator={<SearchIcon />}
                     placeholder={`Buscar`}
                     size={"lg"}
                 />
             </div>
-            <div className={`interests__container--body`}>
-                {new Array(50).fill(0).map((item, index) => (
-                    <div className={`circle`}></div>
+            <div className={`interests__container--body`} style={{ top: '10%', display: 'flex', flexDirection: 'row' }}>
+                {comunidade.map((item, index) => (
+                    <div key={item.id} >
+                        <div className={`circle`}>
+                            <h2>{item.ideologias}</h2>
+                        </div>
+                        <div className={`circle`}>
+                            <h2>{item.experiencias}</h2>
+                        </div>
+                        <div className={`circle`}>
+                            <h2>{item.social}</h2>
+                        </div>
+                        <div className={`circle`}>
+                            <h2>{item.afinidades}</h2>
+                        </div>
+                    </div>
                 ))}
             </div>
         </section>
     );
 }
+

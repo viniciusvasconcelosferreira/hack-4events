4710ccc767d854843f73df954526b9191b124b37
Ravanz <rayteon.h@gmail.com>
2023-04-08 02:07:53 -0300

    feat: incluindo formulário para registro

--- src/form.js
+++ src/form.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-export default function Register() {
+export default function Form() {
   const { register, handleSubmit } = useForm();
 
   return (

af9345fa0c2e6d4f2b27e589e79d3d2f31d3ba61
Ravanz <rayteon.h@gmail.com>
2023-04-08 05:21:36 -0300

    fix: espaçamento do formulário

--- src/pages/Register/register.js
+++ src/pages/Register/register.js
@@ -16,28 +16,33 @@ export default function Register() {
         <div className={`login__container--body__sign-in`}>
           <span className={`login__container--body__sign-in--logo`}><img src={logo}
             alt="Logo da Four Community" /></span>
-          <span className={`login__container--body__sign-in--title`}>Entre com seu email</span>
-          <span className={`login__container--body__sign-in--sub-title`}>Para acessar suas comunidades</span>
           <form>
             <h1>4.community</h1>
             <label>E-mail:</label>
+            <br></br>
             <input name="email" placeholder="ex: voce@email.com" />
             <br></br>
             <label>Primeiro nome:</label>
+            <br></br>
             <input name="firstName" placeholder="ex: João" />
             <br></br>
             <label>Sobrenome:</label>
+            <br></br>
             <input name="lastName" placeholder="ex: Santana" />
             <br></br>
             <label>Data de Nascimento:</label>
+            <br></br>
             <input name="date" type="date" />
             <br></br>
             <label>Senha:</label>
+            <br></br>
             <input name="password" />
-            <input type="submit" />
+            <br></br>
+            <input type="submit" value={SubmitEvent}/>
           </form>
         </div>
       </div>
+      <br></br>
     </section>
   )
 }
